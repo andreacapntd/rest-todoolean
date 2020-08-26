4bb026169be5565790acb077a5a55f0a9b0db53e
andreacapntd <andreacapntd@gmail.com>
2020-08-26 14:57:45 +0200

    impostazione html + css + lettura dati metodo get

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
+function getReadTasks() {
+
+  var target = $('#to_do_list');
+
+  $.ajax({
+
+    url: 'http://157.230.17.132:3002/todos',
+    method: 'GET',
+    success: function(data) {
+      console.log('data', data);
+    },
+    error: function(err) {
+      console.log('err', err);
+    }
+  });
+}
+
+
+
+
 function init() {
 
-  console.log('helloworld');
+  getReadTasks();
+
 }
 
 
