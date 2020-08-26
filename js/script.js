a62525a3cf592ab664296993477c46da43b342e9
andreacapntd <andreacapntd@gmail.com>
2020-08-26 15:18:20 +0200

    metodo get più stampa a schermo ok

--- js/script.js
+++ js/script.js
@@ -1,19 +1,33 @@
 function getReadTasks() {
 
-  var target = $('#to_do_list');
-
   $.ajax({
 
     url: 'http://157.230.17.132:3002/todos',
     method: 'GET',
     success: function(data) {
       console.log('data', data);
+      getPrintTasks(data);
     },
     error: function(err) {
       console.log('err', err);
     }
   });
 }
+
+function getPrintTasks(tasks) {
+
+  var target = $('#to_do_list');
+
+  for (var i = 0; i < tasks.length; i++) {
+
+    var task = tasks[i];
+    var text = task.text;
+    task.text = `<li> <span> ${text} </span> <span> <i class="fas fa-trash-alt"></i> </span> </li> `
+    target.append(task.text);
+
+  }
+
+};
 
 
 
